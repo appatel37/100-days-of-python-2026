7cf51dd15e8d7618ae22cf0ab24703cb866aab79
Aryan Patel <113740200+appatel37@users.noreply.github.com>
2026-03-10 04:44:12 +0000

    Day 01 - completed first Python exercise

---
+++ Day_01/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generaator.")
+
+city = input("What's the name of the city you grew up in?")
+
+name = input("What's your pet's name?")
+
+print("Your band name could be " + city + " " + name)
